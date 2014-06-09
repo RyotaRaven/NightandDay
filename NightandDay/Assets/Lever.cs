df7e74df7ed77aff01cfd7cc7e08803aef34edd8
RyotaRaven <Rachael.metcalf@utexas.edu>
2014-06-09 02:57:51 -0500

    had unadded files

---
+++ NightandDay/Assets/Lever.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : MonoBehaviour {
+
+	public GameObject lever;
+	public Sprite[] newLever;
+	int whichLever=0;
+
+	public bool activated=false;
+
+	void OnTriggerStay2D (Collider2D col)
+	{
+		if (col.collider2D.tag == "Player")
+		{
+			if(Input.GetButtonDown("Fire2"))
+			{
+				if(whichLever==0) {activated=true; whichLever=1;} 
+				else {whichLever=0; activated=false;}
+				lever.GetComponent<SpriteRenderer>().sprite=newLever[whichLever];
+			}
+		}
+	}
+}
---
+++ NightandDay/Assets/WallUnlock.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallUnlock : MonoBehaviour {
+
+	
+	public GameObject[] Triggers;
+	public MeshRenderer[] Obstacle;
+	private int toOpen;
+	private bool[] canOpen;
+	public GameObject[] wall;
+	private bool Open;
+	
+	// Use this for initialization
+	void Start () { 
+		canOpen = new bool[Triggers.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		for (int i=0; i<canOpen.Length; i++)
+			canOpen [i] = Triggers [i].GetComponent<Lever> ().activated;
+		if(canOpen.Length>=2)
+		{
+			Open=canOpen[0] && canOpen[1];
+			for(int x=2; x<canOpen.Length; x++)
+			{
+				Open= Open &&canOpen[x];
+			}
+		}
+		else
+			Open=canOpen[0];
+		if (Open)
+		{
+			foreach(MeshRenderer m in Obstacle)
+			{
+				m.enabled=false;
+			}
+			foreach(GameObject o in wall)
+			{
+				o.collider2D.isTrigger=true;
+			}
+		}
+		else
+		{
+			foreach(MeshRenderer m in Obstacle)
+			{
+				if(m.enabled==false) m.enabled=true;
+			}
+			foreach(GameObject o in wall)
+			{
+				if(o.collider2D.isTrigger==true) o.collider2D.isTrigger=false;
+			}
+		}
+	}
+}
+
