795a3b0bdd06be220a24a98a884ec939d4b5b372
RyotaRaven <Rachael.metcalf@utexas.edu>
2014-06-11 01:47:21 -0500

    respawn

--- NightandDay/Assets/ReSpawn.cs
+++ NightandDay/Assets/ReSpawn.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class ReSpawn : MonoBehaviour {
 
+	float spawnx;
+	float spawny;
+
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		//TODO: set respawn location based on the current level/checkpoint
-		float spawnx = -5.3f;
-		float spawny = -1.0f;
 		if(col.collider2D.tag=="Player") col.transform.position=new Vector2(spawnx, spawny);
+	}
+	public static void setSpawnPoint(float x, float y)
+	{
+		spawnx = x;
+		spawny = y;
 	}
 }
